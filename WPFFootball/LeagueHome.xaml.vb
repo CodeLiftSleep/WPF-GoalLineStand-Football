b6383af6c151057cd37af0c61cca4b5f94f3f53d
CodeLiftSleep <codeliftsleep@yahoo.com>
2016-07-13 22:24:26 -0400

    added milestones and injuries to league homepage

--- WPFFootball/LeagueHome.xaml.vb
+++ WPFFootball/LeagueHome.xaml.vb
@@ -1,21 +1,17 @@
 ﻿Imports System.Data
 Imports System.Globalization
 
-Public  Class LeagueHome
-    Dim ReadOnly MyVM as new LeagueHomeViewModel
-    Private TempDT as New DataTable
-    Sub New
+Public Class LeagueHome
+    ReadOnly MyVM As New LeagueHomeViewModel
+    Private TempDT As New DataTable
+    Sub New()
 
         ' This call is required by the designer.
         InitializeComponent()
         ' Add any initialization after the InitializeComponent() call.
         DataContext = MyVM
         MyVM.Leaguedate = "2/16/16"
-        
-        
+
     End Sub
-
-   
-
 
 End Class
