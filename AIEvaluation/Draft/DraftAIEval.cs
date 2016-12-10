246fd970c2ad27a732398a3e009e29a7eec831e0
CodeLiftSleep <codeliftsleep@yahoo.com>
2016-12-10 01:01:26 -0500

    Changed DB to add in proper Scout Regions for Personnel.  Updated personnel grades

--- AIEvaluation/Draft/DraftAIEval.cs
+++ AIEvaluation/Draft/DraftAIEval.cs
@@ -12,7 +12,11 @@ namespace AIEvaluation.Draft
     //Need to pull grades from the players and then evaluate them and stick them in a DB.  Grades will be updated at various points of the season
 
     public class DraftAIEval
-    {
+    {/// <summary>
+     /// 1) group the players
+     /// 2) group the personnel
+     /// 3) cycle through the groups and the appropriate personnel group grades the appropriate player group
+     /// </summary>
         public DraftAIEval()
         {
             var gradesList = new List<string>();
@@ -26,6 +30,35 @@ namespace AIEvaluation.Draft
                     (from player in draftGroups
                      group player by player.Field<string>("CollegePOS"))
                     group posGroups by draftGroups.Key;
+
+            var personnelDTGroups =  //groups personnel by Scout Region then by PersonnelType, ordered by TeamID
+                   from personnel in PersonnelDT.AsEnumerable()
+                   orderby personnel.Field<int>("TeamID") descending
+                   group personnel by personnel.Field<string>("ScoutRegion") into personnelGroups
+                   from posGroups in
+                   (from personnel in personnelGroups
+                    group personnel by personnel.Field<int>("PersonnelType"))
+                   group posGroups by personnelGroups.Key;
+
+            for (var i = 0; i < 7; i++)
+            {
+                foreach (var region in draftDTGroups)
+                {
+                    foreach (var persReg in personnelDTGroups)
+                    {
+                        if (region.Key == persReg.Key) //regions are the same
+                        {
+                            foreach (var pos in region) //cycle through the positions
+                            {
+                                foreach (var personnel in persReg) //cycle through each scout
+                                {
+                                    GetGrade(pos, personnel, pos.Key);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
 
             foreach (var file in files) //open each file
             {
@@ -44,7 +77,7 @@ namespace AIEvaluation.Draft
 
                     for (int i = 0; i < PersonnelDT.Rows.Count; i++) //cycles through the scouts
                     {
-                        gradeProspects((int)DraftDT.Rows[i]["DraftID"], DraftDT.Rows[i]["ScoutRegion"].ToString(), gradesList);
+                        //gradeProspects((int)DraftDT.Rows[i]["DraftID"], DraftDT.Rows[i]["ScoutRegion"].ToString(), gradesList);
                     }
                 }
             }
@@ -54,13 +87,13 @@ namespace AIEvaluation.Draft
         /// Grades are generated for each prospect by each scout
         /// </summary>
         /// <returns></returns>
-        private void gradeProspects(int playerId, string region, List<string> gradesList)
+       /* private void gradeProspects(int playerId, string region, List<string> gradesList)
         {
             int role;
-            decimal top150 = 0.0m; //grade cutoff for 150th player
-            decimal top250 = 0.0m; //grade cutoff for 250th player
-            decimal top350 = 0.0m; //grade cutoff for 350th player
-            decimal top450 = 0.0m; //grade cutoff for 450th player
+            decimal top150 = 7.5m; //grade cutoff for 150th player
+            decimal top250 = 7.0m; //grade cutoff for 250th player
+            decimal top350 = 6.5m; //grade cutoff for 350th player
+            decimal top450 = 6.35m; //grade cutoff for 450th player
 
             var pos = gradesList[0];
             var playerRole = gradesList[1];
@@ -124,127 +157,223 @@ namespace AIEvaluation.Draft
                     }
                 });
             }
-        }
+        }*/
 
         /// <summary>
-        /// Runs information through the algorithm and determines the grade for the player by that Personnel person
+        /// Sends the personnel and player objects along with the position string to the algorithm.  Cycle through each player and run the grade based on the position
         /// </summary>
         /// <param name="playerID"></param>
         /// <param name="scoutID"></param>
         /// <returns></returns>
-        private decimal GetGrade(int playerId, int personnelId)
+        private decimal GetGrade(IGrouping<int, DataRow> playerGroup, IGrouping<int, DataRow> personnelGroup, string pos)
+        {
+            switch (pos.ToString())
+            {
+                case "QB":
+
+                    foreach (var player in playerGroup) //cycle through each player
+                    {
+                        foreach (var personnel in personnelGroup) //cycle through each personnel
+                        {
+                            GradeQB(player, personnel);
+                        }
+                    }
+                    break;
+
+                case "RB":
+                    foreach (var player in playerGroup) //cycle through each player
+                    {
+                        foreach (var personnel in personnelGroup) //cycle through each personnel
+                        {
+                            GradeRB(player, personnel);
+                        }
+                    }
+                    break;
+
+                case "FB":
+                    foreach (var player in playerGroup) //cycle through each player
+                    {
+                        foreach (var personnel in personnelGroup) //cycle through each personnel
+                        {
+                            GradeFB(player, personnel);
+                        }
+                    }
+                    break;
+
+                case "WR":
+                    foreach (var player in playerGroup) //cycle through each player
+                    {
+                        foreach (var personnel in personnelGroup) //cycle through each personnel
+                        {
+                            GradeWR(player, personnel);
+                        }
+                    }
+                    break;
+
+                case "TE":
+                    foreach (var player in playerGroup) //cycle through each player
+                    {
+                        foreach (var personnel in personnelGroup) //cycle through each personnel
+                        {
+                            GradeTE(player, personnel);
+                        }
+                    }
+                    break;
+
+                case "OT":
+                    foreach (var player in playerGroup) //cycle through each player
+                    {
+                        foreach (var personnel in personnelGroup) //cycle through each personnel
+                        {
+                            GradeOT(player, personnel);
+                        }
+                    }
+                    break;
+
+                case "OG":
+
+                    foreach (var player in playerGroup) //cycle through each player
+                    {
+                        foreach (var personnel in personnelGroup) //cycle through each personnel
+                        {
+                            GradeOG(player, personnel);
+                        }
+                    }
+                    break;
+
+                case "C":
+                    foreach (var player in playerGroup) //cycle through each player
+                    {
+                        foreach (var personnel in personnelGroup) //cycle through each personnel
+                        {
+                            GradeOC(player, personnel);
+                        }
+                    }
+                    break;
+
+                case "DE":
+                    foreach (var player in playerGroup) //cycle through each player
+                    {
+                        foreach (var personnel in personnelGroup) //cycle through each personnel
+                        {
+                            GradeDE(player, personnel);
+                        }
+                    }
+                    break;
+
+                case "DT"
+            }
+            decimal grade = 0m;
+
+            return grade;
+        }
+
+        private decimal GradeQB(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
             return grade;
         }
 
-        private decimal GradeQB(int playerId, int personnelId)
+        private decimal GradeRB(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
             return grade;
         }
 
-        private decimal GradeRB(int playerId, int personnelId)
+        private decimal GradeFB(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
             return grade;
         }
 
-        private decimal GradeFB(int playerId, int personnelId)
+        private decimal GradeWR(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
             return grade;
         }
 
-        private decimal GradeWR(int playerId, int personnelId)
+        private decimal GradeTE(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
             return grade;
         }
 
-        private decimal GradeTE(int playerId, int personnelId)
+        private decimal GradeOT(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
             return grade;
         }
 
-        private decimal GradeOT(int playerId, int personnelId)
+        private decimal GradeOG(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
             return grade;
         }
 
-        private decimal GradeOG(int playerId, int personnelId)
+        private decimal GradeOC(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
             return grade;
         }
 
-        private decimal GradeOC(int playerId, int personnelId)
+        private decimal GradeDE(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
             return grade;
         }
 
-        private decimal GradeDE(int playerId, int personnelId)
+        private decimal GradeDT(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
             return grade;
         }
 
-        private decimal GradeDT(int playerId, int personnelId)
+        private decimal GradeOLB(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
             return grade;
         }
 
-        private decimal GradeOLB(int playerId, int personnelId)
+        private decimal GradeILB(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
             return grade;
         }
 
-        private decimal GradeILB(int playerId, int personnelId)
+        private decimal GradeCB(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
             return grade;
         }
 
-        private decimal GradeCB(int playerId, int personnelId)
+        private decimal GradeSF(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
             return grade;
         }
 
-        private decimal GradeSF(int playerId, int personnelId)
+        private decimal GradeP(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
             return grade;
         }
 
-        private decimal GradeP(int playerId, int personnelId)
-        {
-            decimal grade = 0m;
-
-            return grade;
-        }
-
-        private decimal GradeK(int playerId, int personnelId)
+        private decimal GradeK(DataRow player, DataRow personnel)
         {
             decimal grade = 0m;
 
